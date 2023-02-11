b18c6f0bac5d579c102b8f84856561d2c18f8210
mjj111 <skatks1016@naver.com>
2023-02-11 20:07:09 +0900

    백트레킹은 현재에 추가,넘겨주기, 리셋이 필요++현재의 정보를 담아야한다

--- 백트래킹/1795_암호 만들기.py
+++ 백트래킹/1795_암호 만들기.py
@@ -13,7 +13,7 @@ def back_tracking(now_len,idx) :
         return 
     for s in range(idx,how_many_possible_alpha):
         answer.append(words[s]) # 현재에 추가 
-        back_tracking(now_len+1, s +1)# 다음 녀석으로 넘겨주고 
+        back_tracking(now_len+1, s +1)# 다음 녀석으로 넘겨주고 (현재의 정보 now +1, 현재의 상황 idx +1 )
         answer.pop() #다시 리셋
 
 how_long_alpha, how_many_possible_alpha= map(int,input().split())

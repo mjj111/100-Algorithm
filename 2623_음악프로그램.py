f4398d2041bd1216a06ef8d253fdb9b85655b9a7
mjj111 <skatks1016@naver.com>
2023-02-12 20:05:47 +0900

    위상정렬

---
+++ 2623_음악프로그램.py
@@ -0,0 +1,34 @@
+import sys
+from collections import deque
+#위상정렬
+def topology_sort():
+    result = []
+    q = deque()
+    for i in range(n):
+        if indegree[i] == 0: 
+            q.append(i)
+    while q:
+        now = q.popleft()
+        result.append(now + 1) 
+        for i in graph[now]:
+            indegree[i] -= 1
+            if indegree[i] == 0:  
+                q.append(i)
+
+    if sum(indegree) > 0: 
+        print(0)
+    else:
+        [print(i) for i in result]
+
+n, m = map(int, sys.stdin.readline().split()) 
+indegree = [0] * n
+result = [1] * n
+graph = [[] for i in range(n)]
+
+for _ in range(m):
+    list_ = list(map(int, sys.stdin.readline().split()))
+    for a, b in zip(list_[1:], list_[2:]):
+        graph[a - 1].append(b - 1)  
+        indegree[b - 1] += 1   
+
+topology_sort()

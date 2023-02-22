393bba1ed7a16cb44f5a7d31b0748106afcbaac7
mjj111 <skatks1016@naver.com>
2023-02-22 23:11:27 +0900

    코드수정

--- test.py
+++ test.py
@@ -1,6 +1,34 @@
+import sys 
 from collections import deque
-d = deque()
-d.append([1,2])
-a ,b = d.popleft()
-print(a,b)
-#popleft 리스트로 넣고 뽑어도 된다. 
+
+def bfs(x):
+    deq = deque()
+    deq.append(x)
+    visited[x] = 1
+    result = 0
+    while deq :
+        result += 1
+        for i in range(len(deq)):
+            now_x = deq.popleft()
+            if now_x == wanna_find_y:
+                return result - 1
+            for next_x in people[now_x]:
+                if visited[next_x]  == 0:
+                    visited[next_x] = 1 
+                    deq.append(next_x)
+    return -1
+
+        
+
+
+howmany_people = int(input())
+wanna_find_x,wanna_find_y = map(int, input().split())
+relation = int(input())
+people = [[] for _ in range(howmany_people+1)] # 빈2차원 행렬을 만들기 위해서 사용 
+for i in range(relation):
+    x,y = map(int, input().split())
+    people[x].append(y)
+    people[y].append(x)# 단방향이라면 넣지 않아도 된다
+    
+visited = [0] * (howmany_people + 1) # 빈 1차원 행렬을 만들기 위해 사용 
+print(bfs(wanna_find_x))

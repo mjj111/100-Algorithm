679072ebcc378ea261809bb103f9ef9f3709ee13
김명준 <86913355+mjj111@users.noreply.github.com>
2023-06-15 19:05:00 +0900

    프로그래머스 고득점  kit DP

---
+++ 프로그래머스_고득점Kit/DP/정수삼각형.py
@@ -0,0 +1,13 @@
+# 단순히 비교만 해주면 된다. 
+def solution(triangle):
+    answer = 0
+    for i in range(1, len(triangle)):
+        for j in range(len(triangle[i])):
+            if j == 0:
+                triangle[i][j] += triangle[i-1][j]
+            elif j == len(triangle[i])-1:
+                triangle[i][j] += triangle[i-1][j-1]
+            else:
+                triangle[i][j] += max(triangle[i-1][j-1], triangle[i-1][j])
+    answer = max(triangle[-1])
+    return answer
